62039436aba27aa7fc6e4307e5275c8c57a8de08
yeyuanhua <1378476425@qq.com>
2022-06-16 13:52:55 +0800

    样式

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 export default () => {
   return (
     <div>
-      <p>视频:开启了跨域隔离导致第三方网站的文件不能访问如阿里巴巴在线图标库</p>
+      <h2>项目存在未解决的问题</h2>
+      <p>1、视频:开启了跨域隔离导致第三方网站的文件不能访问如阿里巴巴在线图标库</p>
     </div>
   );
 };
